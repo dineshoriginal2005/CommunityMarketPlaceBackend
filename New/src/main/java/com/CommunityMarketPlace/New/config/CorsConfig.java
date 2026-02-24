24efa3ce2ce9e647a09e215e0c454be87e9d7bfe
Dinesh R <dineshoriginal2005@gmail.com>
2026-02-24 13:00:41 +0530

    Fix: Updated frontend configuration and repository link

--- New/src/main/java/com/CommunityMarketPlace/New/config/CorsConfig.java
+++ New/src/main/java/com/CommunityMarketPlace/New/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints in your API
-                .allowedOrigins("https://opermart-theta.vercel.app") // Replace with your actual Vercel URL
+                .allowedOrigins("https://openmart-theta.vercel.app") // Replace with your actual Vercel URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow standard HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow sending of cookies or auth headers if needed
