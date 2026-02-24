167d29424299b4b5b089ed514b23eacd61a393c7
Dinesh R <dineshoriginal2005@gmail.com>
2026-02-24 12:34:21 +0530

    Fix: Corrected package name for CorsConfig

--- New/src/main/java/com/CommunityMarketPlace/New/config/CorsConfig.java
+++ New/src/main/java/com/CommunityMarketPlace/New/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.yourproject.config;
+package com.CommunityMarketPlace.New.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
